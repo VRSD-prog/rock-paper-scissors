e68d2ee7c6b9e5cea35c0701f3f57507f974ba5a
VRSD-prog <cyreeka2016@gmail.com>
2025-05-30 17:59:04 +0800

    Created Initial Source Code

--- script.js
+++ script.js
@@ -1,65 +1,143 @@
+//Global Variables
+let humanScore = 0
+let computerScore = 0
+
+const playerPoints = document.querySelector(".playerPoints")
+const computerPoints = document.querySelector(".computerPoints")
+const buttons = document.querySelector(".buttons")
+const gameResult = document.querySelector(".gameResult")
+
+playerPoints.textContent = `${humanScore}`
+computerPoints.textContent = `${computerScore}`
+
 // Computer Choice Generation
 function getComputerChoice(){
     return Math.floor(Math.random() * 3)
 }
 
-// Human's Input
-function getHumanChoice(){
-    return prompt("Enter R, P, or S (Rock, Paper, Scissors)").toUpperCase()
-}
-
-let humanScore = 0
-let computerScore = 0
-
-
 // Game Logic
-function playRound(computerChoice, humanChoice){
+function playRound(playerSelection){
 
-    computerChoice = getComputerChoice()
-    humanChoice = getHumanChoice()
+    console.log(playerSelection)
+
+    let computerChoice = getComputerChoice()
+    let humanChoice = playerSelection
 
     if (computerChoice === 0 && humanChoice === "R" || computerChoice === 1 && humanChoice === "P" || computerChoice === 2 && humanChoice === "S"){
-        alert("Tie!")
         return "Tie"
     } else if (computerChoice === 0 && humanChoice === "P" || computerChoice === 1 && humanChoice === "S" || computerChoice === 2 && humanChoice === "R"){
-        alert("You Win!")
         return "Win"
     } else {
-        alert("You Lose!")
         return "Lose"
     }
 }
 
-
 // Main Program
 function playGame(){
 
-    while ("Nigga"){
+    const rockButton = document.createElement("button")
+    const paperButton = document.createElement("button")
+    const scissorsButton = document.createElement("button")
+    rockButton.textContent = "Rock"
+    paperButton.textContent = "Paper"
+    scissorsButton.textContent = "Scissors"
+    buttons.appendChild(rockButton)
+    buttons.appendChild(paperButton)
+    buttons.appendChild(scissorsButton)
 
-        let result = playRound()
+    gameResult.textContent = "Choose Your Move!"
 
-        if (result === "Win"){
-            humanScore += 1
-            console.log(humanScore)
-            console.log(computerScore)
-        } else if (result === "Lose"){
-            computerScore += 1
-            console.log(humanScore)
-            console.log(computerScore)
-        } else {
-            console.log(humanScore)
-            console.log(computerScore)
-        }
+    playerPoints.style.fontWeight = "400"
+    computerPoints.style.fontWeight = "400"
 
-        if (humanScore === 3){
-            alert("Player Wins!")
-            break
-        } else if (computerScore === 3){
-            alert("Computer Wins!")
-            break
-        }
+    playerPoints.textContent = "0"
+    computerPoints.textContent = "0"
+        
+    let playerSelection
+    let result
+
+    rockButton.addEventListener("click", () => {
+        playerSelection = "R"
+        result = playRound(playerSelection)
+        updateScores()
+    })
+
+    paperButton.addEventListener("click", () => {
+        playerSelection = "P"
+        result = playRound(playerSelection)
+        updateScores()
+    })
+
+    scissorsButton.addEventListener("click", () => {
+        playerSelection = "S"
+        result = playRound(playerSelection)
+        updateScores()
+    })
+
+
+function updateScores() {
+    if (result === "Win"){
+        gameResult.textContent = "Won!"
+        humanScore += 1
+        playerPoints.textContent = `${humanScore}`
+        computerPoints.textContent = `${computerScore}`
+        console.log(result)
+        console.log(humanScore)
+        console.log(computerScore)
+    } else if (result === "Lose"){
+        gameResult.textContent = "Lost!"
+        computerScore += 1
+        playerPoints.textContent = `${humanScore}`
+        computerPoints.textContent = `${computerScore}`
+        console.log(result)
+        console.log(humanScore)
+        console.log(computerScore)
+    } else {
+        gameResult.textContent = "Tie!"
+        playerPoints.textContent = `${humanScore}`
+        computerPoints.textContent = `${computerScore}`
+        console.log(result)
+        console.log(humanScore)
+        console.log(computerScore)
+    }
+
+    if (humanScore === 3){
+        gameResult.textContent = "Player Wins!"
+        playerPoints.style.fontWeight = "900"
+
+        rockButton.remove()
+        scissorsButton.remove()
+        paperButton.remove()
+
+        const replayButton = document.createElement("button")
+        buttons.appendChild(replayButton)
+        replayButton.textContent = "Play Again"
+        replayButton.addEventListener("click", () => {
+            humanScore = 0
+            computerScore = 0
+            replayButton.remove()
+            playGame()
+        })
+    } else if (computerScore === 3){
+        gameResult.textContent = "Computer Wins!"
+        computerPoints.style.fontWeight = "900"
+
+        rockButton.remove()
+        scissorsButton.remove()
+        paperButton.remove()
+
+        const replayButton = document.createElement("button")
+        buttons.appendChild(replayButton)
+        replayButton.textContent = "Play Again"
+        replayButton.addEventListener("click", () => {
+            humanScore = 0
+            computerScore = 0
+            replayButton.remove()
+            playGame()
+        })
     }
 }
+}
 
-// Start Program
+// Start
 playGame()
